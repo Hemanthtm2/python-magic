4b4b30c6fac637fdc3f1c5df3f3369f86d433b3c
hemanthtm2 <hemanthtm2@gmail.com>
2019-08-19 10:32:11 +0000

    list sum

---
+++ listsum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python 
+
+my_list=list(range(1,11))
+list_sum=0
+
+for num in my_list:
+    list_sum=list_sum+num
+
+print(list_sum)
+
+
---
+++ testif.py
@@ -0,0 +1,6 @@
+#/usr/bin/python 
+name='Hemanth'
+if name=='Hemanth':
+    print(True)
+else:
+    print(False)
