4c3e0eb80438bfeb918863eca9caa924dc090c8c
hemanthtm2 <hemanthtm2@gmail.com>
2020-02-12 21:43:24 +0000

    Python magic functions

---
+++ magicfun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python 
+
+
+class Book():
+
+      def __init__(self,title,author,pages):
+          self.title=title
+          self.author=author
+          self.pages=pages
+      def __str__(self):
+          return f"{self.title} by {self.author}"
+     
+      def __len__(self):
+          return self.pages
+      def __del__(self):                                   #invoke only when we deleted an object/variable that is 'del b' in this case
+           print("A book object has been deleted")
+
+
+b=Book("Python rocks!!!","Hemanth",200)
+
+print(b)
+
+print(len(b))
